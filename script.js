35eeb20bd2ad15cb980470f6cf3550ba475c10d7
Glitch (carbonated-patch-paneer) <none>
2021-03-21 22:46:57 +0000

    Third Commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const cluePauseTime = 333; //how long to pause in between clues
 const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
 
 //Global Variables
-var pattern = [2, 5, 4, 3, 2, 1, 2, 4, 5, 3, 5];
+var pattern = [2, 5, 4, 3, 2, 1, 2, 4, 5, 3];
 var progress = 0; 
 var gamePlaying = false;
 var tonePlaying = false;
